0389ef5a66c7652edd33290b601f989f71a358f4
Gundu Raju (MS/EBB8-VM) <Raju.Gundu@bosch.com>
2024-10-22 17:43:50 +0530

    Implemented header component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <main className="bg-[#1a191e] h-screen">
+       {/* <Header />  */}
+      <Home></Home>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import HeaderNav from "./HeaderNav";
+
+export default function Header() {
+  let navStyle =
+    "text-xl p-2 font-semibold text-slate-500 hover:text-slate-700 hover:bg-[#e3e3e3] cursor-none rounded-md";
+  return (
+    <>
+      <div className="flex justify-between items-center p-1">
+        <HeaderNav
+          href="#home"
+          className="text-xl p-2 font-bold text-slate-200 ml-16 hover:bg-[#e3e3e3] hover:text-slate-600 cursor-none rounded-md"
+        >
+          Raju Gundu
+        </HeaderNav>
+        <nav className="flex-1 flex space-x-6 justify-center">
+          <HeaderNav href="#info" className={navStyle}>Info</HeaderNav>
+          <HeaderNav href="#projects" className={navStyle}>Projects</HeaderNav>
+          <HeaderNav href="#experience" className={navStyle}>Experience</HeaderNav>
+        </nav>
+      </div>
+      <hr className="mt-2 w-11/12 mx-auto border-slate-500" />
+    </>
+  );
+}
